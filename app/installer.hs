2250b8807409882dfe8ac66a9f5ca21df064ddf9
Michael Snoyman <michael@snoyman.com>
2021-05-14 10:08:10 +0300

    Download Stack and Cabal, include in installer

--- app/installer.hs
+++ app/installer.hs
@@ -1,9 +1,16 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 import RIO
+import Haskup (runHaskup)
 import Development.NSIS
 import Development.NSIS.Plugins.EnvVarUpdate
+import Network.HTTP.Download (verifiedDownload, mkDownloadRequest, setLengthCheck, CheckHexDigest (CheckHexDigestString), HashCheck (HashCheck), setHashChecks)
+import Path (Path, Abs, Rel, File, Dir, mkRelFile, mkRelDir, (</>))
+import Path.IO (makeAbsolute)
+import qualified Crypto.Hash.Algorithms
 
 -- Note that it is *required* to use a NSIS compiler that supports long strings,
 -- to avoid corrupting the user's $PATH.
@@ -11,10 +18,44 @@ import Development.NSIS.Plugins.EnvVarUpdate
 executables :: [FilePath]
 executables =
   [ "haskup.exe"
+  , "cabal.exe"
+  , "stack.exe"
+  ]
+
+data Download = Download
+  { url :: !String
+  , size :: !Int
+  , sha256 :: !String
+  , filename :: !(Path Rel File)
+  }
+
+downloads :: [Download]
+downloads =
+  [ Download
+      { url = "https://github.com/commercialhaskell/stack/releases/download/v2.7.1/stack-2.7.1-windows-x86_64-bin.exe"
+      , size = 59980288
+      , sha256 = "1a15494065de2f72fe1e35d208f2c3c874801ef92df3c8804e910e1de0a93f05"
+      , filename = $(mkRelFile "stack.exe")
+      }
+  , Download
+      { url = "https://s3.amazonaws.com/download.fpcomplete.com/cabal/cabal-install-3.4.0.0-x86_64-windows.exe"
+      , size = 87932677
+      , sha256 = "5effd1993aff5e5825bd408a1ec1aa382ffd59b511bc1680a0527f3dbcc173e7"
+      , filename = $(mkRelFile "cabal.exe")
+      }
   ]
 
 main :: IO ()
-main = do
+main = runHaskup $ do
+  destDir :: Path Abs Dir <- makeAbsolute ($(mkRelDir "bin") :: Path Rel Dir)
+  for_ downloads $ \download -> do
+    let dreq = setHashChecks [HashCheck Crypto.Hash.Algorithms.SHA256 (CheckHexDigestString (sha256 download))]
+             $ setLengthCheck (Just (size download))
+             $ mkDownloadRequest
+             $ fromString
+             $ url download
+    let dest = destDir </> (filename download)
+    void $ verifiedDownload dreq dest (\_ -> pure ())
   writeFileUtf8 "bin/haskup-installer.nsi" $ fromString $ nsis $ do
     _ <- constantStr "Name" "haskup"
 
